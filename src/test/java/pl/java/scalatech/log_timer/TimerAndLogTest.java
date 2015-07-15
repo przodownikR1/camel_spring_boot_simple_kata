b171a7561da5cd917d822280c0734a7d7125116f
Slawomir Borowiec <przodownik@tlen.pl>
2015-07-15 13:33:02 +0200

    move nbp to different project https://github.com/przodownikR1/nbpExchangeRatesMicroService.git

--- src/test/java/pl/java/scalatech/log_timer/TimerAndLogTest.java
+++ src/test/java/pl/java/scalatech/log_timer/TimerAndLogTest.java
@@ -10,14 +10,12 @@ import org.apache.camel.model.ModelCamelContext;
 import org.apache.camel.processor.interceptor.Tracer;
 import org.junit.Test;
 
-import pl.java.scalatech.nbp.FileContentLoader;
-
 @Slf4j
 public class TimerAndLogTest {
     @Test
     public void shouldReadAndParseXmlFileWork() throws Exception {
         SimpleRegistry registry = new SimpleRegistry();
-        registry.put("fileContentReader", new FileContentLoader());
+
         ModelCamelContext context = new DefaultCamelContext(registry);
 
         Tracer tracer = new Tracer();
@@ -45,9 +43,9 @@ public class TimerAndLogTest {
 
         @Override
         public void configure() throws Exception {
-            from("quartz2://myGroup/przodownik?cron=*+*+10-18+?+*+MON-FRI").transform().simple("####MESSAGE### -> ${header.firedTime}")
-                    .process(exchange -> log.info("headers {}", exchange.getIn().getHeaders()))
-                    .log(LoggingLevel.INFO, "myCamel", " next : ${header.nextFireTime} , jobDetail : ${header.jobDetail}")
+            from("quartz2://myGroup/przodownik?cron=*+*+10-18+?+*+MON-FRI").transform().simple("####MESSAGE### -> ${header.firedTime}").setHeader("type")
+                    .constant("customer").process(exchange -> log.info("headers {}", exchange.getIn().getHeaders()))
+                    .log(LoggingLevel.INFO, "myCamel", " next : ${header.nextFireTime}   , type : ${header.type}")
                     .to("log:myCamel?level=INFO&showAll=true&multiline=true");
         }
     }

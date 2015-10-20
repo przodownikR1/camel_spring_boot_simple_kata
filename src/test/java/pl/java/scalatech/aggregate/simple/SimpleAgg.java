89a08242572b11968e1b86290230d8e77f0bc147
Slawomir Borowiec <przodownik@tlen.pl>
2015-10-20 16:20:39 +0200

    splitter simple

--- src/test/java/pl/java/scalatech/aggregate/simple/SimpleAgg.java
+++ src/test/java/pl/java/scalatech/aggregate/simple/SimpleAgg.java
@@ -14,38 +14,32 @@ public class SimpleAgg implements AggregationStrategy {
 
     @Override
     public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
-      /*  if (oldExchange != null) {
-            log.info("OLD :  {}", oldExchange.getIn().getBody());
-        }
-        log.info("NEW  :  {}", newExchange.getIn().getBody());*/
+        /*
+         * if (oldExchange != null) {
+         * log.info("OLD :  {}", oldExchange.getIn().getBody());
+         * }
+         * log.info("NEW  :  {}", newExchange.getIn().getBody());
+         */
         List<String> result = newArrayList();
         String id = newExchange.getIn().getBody(String.class);
         saveIntoDb(id);
         if (oldExchange == null) {
-            
             result.add(id);
             newExchange.getIn().setBody(result);
             return newExchange;
-
         }
-       
         result = oldExchange.getIn().getBody(List.class);
         result.add(id);
-
         return oldExchange;
     }
 
-
-
-     private void saveIntoDb(String id) {
-        log.info("insert into db  :  {} ",id );
+    private void saveIntoDb(String id) {
+        log.info("insert into db  :  {} ", id);
     }
 
+    public void info(List<String> agg) {
+        log.info("update ids : {}", agg);
 
-
-    public  void info(List<String> agg){
-        log.info("update ids : {}",agg);     
- 
-     }
+    }
 
 }
